0ff0ae1ce8b7f248c2f7c306067fd70d7fe94ec7
Jerome Caporossi <jerome.caporossi@gmail.com>
2020-01-29 12:27:11 +0100

    Answer to exercise 1.1.3

---
+++ chapter1/exercise1.1.3/TestPalindrome.js
@@ -0,0 +1,28 @@
+function estPalindrome(string) {
+	let start = 0;
+	let end = string.length - 1;
+	while ((string[start] == string[end]) && (start <= end))
+	{
+		start += 1;
+		end -= 1;
+		if (string[start] == " ")
+			start += 1;
+		if (string[end] == " ")
+			end -= 1;
+	}
+	if (start > end)
+		return true;
+	else
+		return false;
+}
+
+function estPalindrome2(str) {
+	str = str.split(" ").join("");
+    return str === str.split("").reverse().join("");
+}
+
+
+
+console.log(estPalindrome("ASSIETTE"));
+console.log(estPalindrome("ESOPE RESTE ICI ET SE REPOSE"));
+let 
