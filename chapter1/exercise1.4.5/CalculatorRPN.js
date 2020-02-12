bd0fdddbe0704e78485fae3d8da62e5173ceb541
Jerome Caporossi <jerome.caporossi@gmail.com>
2020-02-12 14:42:28 +0100

    Answer to exercise 1.4.5

---
+++ chapter1/exercise1.4.5/CalculatorRPN.js
@@ -0,0 +1,58 @@
+let stack =[];
+
+let s ="12 3 * 34 +";
+
+class RPNCalc {
+	constructor(){
+		this.stack = [];
+		stack.push(0);
+	}
+
+	add(s){
+		let tab = s.split(" ");
+		let a, b;
+		while (tab.length > 0) {
+			let symbol = tab.shift();
+			switch(symbol){
+				case '*':
+					b = this.stack.pop();
+					a = this.stack.pop();
+					this.stack.push(a*b);
+					break;
+				case '/':
+					b = this.stack.pop();
+					a = this.stack.pop();
+					this.stack.push(a/b);
+					break;
+				case '+':
+					b = this.stack.pop();
+					a = this.stack.pop();
+					this.stack.push(a+b);
+					break;
+				case '-':
+					b = this.stack.pop();
+					a = this.stack.pop();
+					this.stack.push(a-b);
+					break;
+				default:
+					this.stack.push(parseInt(symbol));
+					break;
+			}
+		}
+	}
+
+	pop(){
+		return this.stack[this.stack.length - 1];
+	}
+
+	flush() {
+		while (this.stack.length > 1)
+			this.stack.pop();
+		this.stack.push(0);
+	}
+}
+
+let calc = new RPNCalc();
+calc.add("12 3 * 5 -");
+calc.add("2 * 5 +");
+console.log(calc.pop());
