05b9dae93a2e3ff575752b58270b761b26198bd4
Jerome Caporossi <jerome.caporossi@gmail.com>
2020-03-30 17:45:27 +0200

    Answer to exercise 4.2.2

--- chapter4/exercise4.2.2/application.js
+++ chapter4/exercise4.2.2/application.js
@@ -1,40 +1,187 @@
+const credibility_sc = {
+
+	address : "0x94Ed722604Bca2909fb8A373Ba12885e47b85Ebb",
+	abi : [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "cred",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"name": "hashs",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "ordre",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "url",
+				"type": "string"
+			}
+		],
+		"name": "produireHash",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "dev",
+				"type": "bytes32"
+			}
+		],
+		"name": "remettre",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "destinataire",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "valeur",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+   ]
+}
+
+
 async function createMetaMaskDapp() {
  try {
    // Demande à MetaMask l'autorisation de se connecter
    const addresses = await ethereum.enable();
-   const address = addresses[0]
+   const user = addresses[0]
    // Connection au noeud fourni par l'objet web3
    const provider = new ethers.providers.Web3Provider(ethereum);
-   dapp = { address, provider };
-   console.log(dapp);
+
+	dapp = { user, provider };
+
  } catch(err) {
    // Gestion des erreurs
    console.error(err);
  }
 }
 
-
-document.getElementById('get_balance').addEventListener('click', event => {
-	dapp.provider.getBalance(dapp.address).then((balance) => {
+async function getBalance(){
+	dapp.provider.getBalance(dapp.user).then((balance) => {
 		let etherString = ethers.utils.formatEther(balance);
-   		console.log("Balance: " + etherString);
+   		console.log("Balance: "+etherString);
    		document.getElementById('balance').innerHTML = etherString;
-	});
- })
+   	});
+}
 
-
-document.getElementById('get_block_number').addEventListener('click', event => {
+async function getBlockNumber(){
 	dapp.provider.getBlockNumber().then((_blockNumber) => {
-		console.log("Block Number " + _blockNumber);
+		console.log("Block Number: " + _blockNumber);
 		document.getElementById('block_number').innerHTML = _blockNumber;
 	});
- })
+}
 
-
-document.getElementById('get_gas_price').addEventListener('click', event => {
+async function getGasPrice(){
 	dapp.provider.getGasPrice().then((_gasPrice) => {
-    	console.log("Gas Price" + _gasPrice);
+    	console.log("Gas Price: " + _gasPrice);
     	let gasPriceString = _gasPrice.toString() + " wei";
     	document.getElementById('gas_price').innerHTML = gasPriceString;
 	});
- })
+}
+
+async function sendHomework(){
+
+	let contratCredibilite = new ethers.Contract(credibility_sc.address, credibility_sc.abi, dapp.provider.getSigner(dapp.user.address));
+
+	let url = document.getElementById('homework_url').value;
+	
+	contratCredibilite.produireHash(url).then((hash) => {
+		console.log("Hash = " + hash.toString());
+		let text = "Homework URL Hash = " + hash;
+		document.getElementById('homework_hash').innerHTML = text;  	
+
+		contratCredibilite.remettre(hash).then((ordre) => {
+			console.log("Ordre = " + ordre.toString());
+		});
+	});
+}
+	
+async function getCredibility(){
+
+	let contratCredibilite = new ethers.Contract(credibility_sc.address, credibility_sc.abi, dapp.provider);
+
+	contratCredibilite.cred(dapp.user).then((credibility) => {
+		let text = "Credibility = " + credibility
+		console.log(text);
+		document.getElementById('credibility').innerHTML = text;
+	});
+
+}
+		
+
+
+
+
